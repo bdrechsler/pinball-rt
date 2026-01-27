2fd11a03c83976c5ae2534172bbb82e6db0dcbf3
bdrechsler <wbd814@gmail.com>
2026-01-27 16:47:42 -0500

    testing disk

--- pinballrt/densities/flared_disk.py
+++ pinballrt/densities/flared_disk.py
@@ -4,9 +4,9 @@ from scipy.integrate import nquad
 
 class FlaredDisk():
 
-    def __init__(self, mass=1e-4*u.Msun, rmin=0.1*u.au, rmax=100*u.au, p=-1, beta=1.25, 
+    def __init__(self, mass=1e-4*u.Msun, rmin=0.1*u.au, rmax=100*u.au, p=-1., beta=1.25, 
                  r_0=1*u.au, h_0=0.1*u.au):
-        self.mass = mass # gas mass
+        self.mass = mass / 100.
         self.rmin = rmin
         self.rmax = rmax
         self.p = p
@@ -19,7 +19,9 @@ class FlaredDisk():
     
     def surface_density(self, r):
         sigma0 = ((2 - self.gamma) * self.mass) / (2 * np.pi * self.rmax**2)
-        return sigma0 * (r/self.rmax)**(-self.gamma) * np.exp(-(r/self.rmax)**(2-self.gamma))
+        sigma = (sigma0 * (r/self.rmax)**(-self.gamma) * np.exp(-(r/self.rmax)**(2-self.gamma))).to(u.g/u.cm**2)
+        sigma[r < self.rmin] = 0.0
+        return sigma
     
     def scale_height(self, r):
         return self.h_0 * (r / self.r_0)**self.beta
@@ -27,13 +29,13 @@ class FlaredDisk():
     def density(self, r, z):
         sigma = self.surface_density(r)
         h = self.scale_height(r)
-        return (sigma / (np.sqrt(2*np.pi)*h)*np.exp(-0.5*(z/h)**2))
+        return (sigma / (np.sqrt(2*np.pi)*h)*np.exp(-0.5*(z/h)**2)).to(u.g/u.cm**3)
     
     def density_grid(self, grid):
         # get grid walls
-        w1 = np.array(grid.grid.w1)
-        w2 = np.array(grid.grid.w1)
-        w3 = np.array(grid.grid.w3)
+        w1 = grid.grid.w1.numpy()
+        w2 = grid.grid.w2.numpy()
+        w3 = grid.grid.w3.numpy() 
 
         # get grid centers
         x1 = (w1[:-1] + w1[1:]) / 2.
@@ -46,13 +48,16 @@ class FlaredDisk():
         # transform the grid coordinates if necessary
         if coordsys == "spherical":
             rt, tt, pp = np.meshgrid(x1, x2, x3, indexing='ij')
-            rr = rt*np.sin(tt)
-            zz = rt*np.cos(tt)
+            rr = rt*np.sin(tt) * u.au
+            zz = rt*np.cos(tt) * u.au
         elif coordsys == "cartesian":
             xx, yy, zz = np.meshgrid(x1, x2, x2, indexing='ij')
-            rr = np.sqrt(xx**2 + yy**2)
+            rr = np.sqrt(xx**2 + yy**2) * u.au
+            zz *= u.au
         elif coordsys == "cylindrical":
             rr, pp, zz = np.meshgrid(x1, x2, x2, indexing='ij')
+            rr += u.au
+            zz *= u.au
 
         # calculate the density grid
         return self.density(rr, zz)
